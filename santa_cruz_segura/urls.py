5a64ef5e5d7f80b51fdbe873d519d767a82d71d4
SrHeath <llleduardolll1@gmail.com>
2026-06-03 01:01:16 -0400

    Restore media_serve + add coordenada validation (Santa Cruz bounding box) + debug logs

--- santa_cruz_segura/urls.py
+++ santa_cruz_segura/urls.py
@@ -1,5 +1,18 @@
+import mimetypes
+import os
+
+from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.http import FileResponse, Http404
+from django.urls import include, path, re_path
+
+def media_serve(request, path):
+    file_path = os.path.normpath(os.path.join(settings.MEDIA_ROOT, path))
+    media_root = os.path.normpath(str(settings.MEDIA_ROOT))
+    if not os.path.exists(file_path) or not file_path.startswith(media_root + os.sep):
+        raise Http404
+    content_type, _ = mimetypes.guess_type(file_path)
+    return FileResponse(open(file_path, 'rb'), content_type=content_type or 'application/octet-stream')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +21,8 @@ urlpatterns = [
     path('estadisticas/', include('estadisticas.urls')),
     path('predicciones/', include('predicciones.urls')),
     path('alertas/', include('alertas.urls')),
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', media_serve),
 ]
